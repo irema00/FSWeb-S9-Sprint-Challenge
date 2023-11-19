40c1cc8cb75080cd954b83fb7da592338921c5b3
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-19 23:19:14 +0300

    adds test for coordinate changes

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -14,3 +14,16 @@ test("Başlangıçta Aktif Kare dökümanda mı?", () => {
   expect(activeSquare).toBeInTheDocument();
   expect(activeSquare.className).toContain("active");
 });
+
+test("Koordinat Değişimi", () => {
+  const coordinates = screen.getByText(/Koordinatlar/);
+  fireEvent.click(screen.getByText("YUKARI"));
+  expect(coordinates).toHaveTextContent("Koordinatlar (2, 1)");
+  fireEvent.click(screen.getByText("SOL"));
+  expect(coordinates).toHaveTextContent("Koordinatlar (1, 1)");
+  fireEvent.click(screen.getByText("SAĞ"));
+  expect(coordinates).toHaveTextContent("Koordinatlar (2, 1)");
+  fireEvent.click(screen.getByText("AŞAĞI"));
+  expect(coordinates).toHaveTextContent("Koordinatlar (2, 2)");
+});
+
