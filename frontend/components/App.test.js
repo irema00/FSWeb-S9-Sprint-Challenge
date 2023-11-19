04c703b21f6bd898f3ebb8ef7e6d18c31f4619f2
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-19 23:17:31 +0300

    adds test for rendering correctly

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,3 +2,6 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import AppFunctional from "./AppFunctional";
+test("hata olmadan render ediliyor", () => {
+  render(<AppFunctional />);
+});
