662855eeab3e5af79481d7e7de713eaa5a3cc6d3
İrem Akın Tunga <iremakin00@gmail.com>
2023-11-19 23:16:57 +0300

    import libs

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,4 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppFunctional from "./AppFunctional";
